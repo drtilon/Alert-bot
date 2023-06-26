88baf3b80ab05f4e903a26487477d2e65e7ab3f2
daniel <kigdaniel04@gmail.com>
2023-06-26 23:37:02 +0300

    last touch-ups

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import json
 from web3 import Web3
 import os
 from twilio.rest import Client
+import time
 
 config = json.load(open('config.json'))
 
@@ -14,20 +15,22 @@ def check_provider(tlos_providers):
 
 def main():
     tlos_provider = check_provider(config["telos_mainnet"])
-    # get_balance fun return tlos price without point, so needed to devide by "point"
+    # get_balance fn return tlos price without dot, so needed to devide by "point"
     point = 1000000000000000000
     # user’s environmental variables
     ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
     AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    # a loop that will go over all the account and will check if the balance is sufficient
-    for i in range(6):
-        if(config["min_amount_tlos"][i] > tlos_provider.eth.get_balance(config["address"][i]) / point):
-            # from - number of sender / body - the body of the message / to - number of receiver
+    # a loop that will go over all the accounts and will check if the balance is sufficient enough
+    addresses = config["addresses"]
+
+    for name, address_price in addresses.items():
+        if(address_price[1] > tlos_provider.eth.get_balance(address_price[0]) / point):
+            # from = number of sender / body = the body of the message / to = number of receiver
             message = client.messages.create(
                 from_='+XXXXXXXX',
-                body='hello your balance in '+ str(config["account_name"][i])+" is insufficient",
+                body='hello your balance in '+ str(name)+" is insufficient",
                 to='+XXXXXXXXX'
             )
             print(message.sid)
@@ -35,3 +38,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    time.sleep(config["interval_sec"])
